174fcf355b0cd881165d047533541cd3462ace4e
Roshan Ravi <roshanravi10@gmail.com>
2017-01-06 19:13:43 -0500

    Emojis are converted to [X] and don't wreak havoc

--- www/js/transfer.js
+++ www/js/transfer.js
@@ -10,26 +10,26 @@ const dataTransfer = {
 	
 	// Sends JSON Document to Hub
 	send: function(config, message){
+		var data = JSON.stringify({
+			// Sends Serial Number for Identification
+			sender: {
+				serial: device.serial	
+			},
+
+			// Core Message to Be Sent
+			msg: message,
+
+			// `CHECK` property is added and verified at receiver
+			//  - utilized to ensure that JSON doc is readable and 
+			//      not lost in transit
+			check: "9dcec4e5sd7f890s"
+		});
+		
+        data = dataTransfer.removeInvalidChars(data);
+        
 		window.plugins.simpleFile.external.write(
-			// File Path
-			config.sendFolder + dataTransfer.tempFilename(), 
-			
-			// JSON Message
-			JSON.stringify({
-				// Sends Serial Number for Identification
-				sender: {
-					serial: device.serial	
-				},
-				
-				// Core Message to Be Sent
-				msg: message,
-				
-				// `CHECK` property is added and verified at receiver
-				//  - utilized to ensure that JSON doc is readable and 
-				//      not lost in transit
-				check: "9dcec4e5sd7f890s"
-			}),
-			
+            config.sendFolder + dataTransfer.tempFilename(), // File Path
+			data, // JSON Message
 			function(){}, // Success Handler
 			dataTransfer.handleError // Error Handler
 		);
@@ -49,5 +49,17 @@ const dataTransfer = {
 
 			return v.toString(16);
 		});
-	}
+	},
+    
+    // Removes Invalid Characters, Such as Emojis
+    // http://stackoverflow.com/a/38987183
+    removeInvalidChars: function(str) {
+        var ranges = [
+          '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
+          '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
+          '\ud83d[\ude80-\udeff]'  // U+1F680 to U+1F6FF
+        ];
+
+        return str.replace(new RegExp(ranges.join('|'), 'g'), '[X]');
+    }
 };
